f8a6bdfa93df5da8c595fa53c80d82ceabcae585
NaveenReddy0427 <naveendeva909@gmail.com>
2024-08-18 21:05:14 +0530

    change Firm input style

--- src/vendorDashboard/components/forms/AddFirm.jsx
+++ src/vendorDashboard/components/forms/AddFirm.jsx
@@ -5,17 +5,55 @@ const AddFirm = () => {
         <form className="tableForm">
             <h3>Add Firm</h3>
             <label>Firm Name</label>
-            <input type="text" placeholder="Enter your firm name"/><br/>
+            <input type="text" /><br/>
             <label>Area</label>
-            <input type="text" placeholder="Enter your area"/><br/>
-            <label>Category</label>
-            <input type="text" placeholder="Enter your category"/><br/>
-            <label>Region</label>
-            <input type="text" placeholder="Enter your category"/><br/>
+            <input type="text" /><br/>
+
+            <div className="checkInp">
+                <label >Category</label>
+                <div className="inputsContainer">
+                    <div className="checboxContainer">
+                        <label>Veg</label>
+                        <input type="checkbox" value="veg" />
+                    </div>
+                    <div className="checboxContainer">
+                        <label>Non-Veg</label>
+                        <input type="checkbox" value="non-veg" />
+                    </div>
+                </div>
+            </div>
+
             <label>offer</label>
-            <input type="text" placeholder="Enter your category"/><br/>
+            <input type="text" /><br/>
+
+            <div className="checkInp">
+                <label >Region</label>
+                <div className="inputsContainer">
+                    <div className="regBoxContainer">
+                        <label>South Indian</label>
+                        <input type="checkbox" value="south-indian"   
+                        />
+                    </div>
+                    <div className="regBoxContainer">
+                        <label>North-Indian</label>
+                        <input type="checkbox" value="north-indian"  
+                        />
+                    </div>
+                    <div className="regBoxContainer">
+                        <label>Chinese</label>
+                        <input type="checkbox" value="chinese" 
+                        />
+                    </div>
+                    <div className="regBoxContainer">
+                        <label>Bakery</label>
+                        <input type="checkbox" value="bakery" 
+                        />
+                    </div>
+                </div>
+            </div>
+            
             <label>Firm Image</label>
-            <input type="file" placeholder="Enter your category"/><br/>
+            <input type="file" /><br/>
             <div className="btnSubmit">
                 <button>Submit</button>
             </div>
