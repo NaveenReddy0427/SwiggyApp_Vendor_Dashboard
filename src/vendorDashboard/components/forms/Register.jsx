3ae8dd07d24df71c66a6ac8d4160849d6fb3f534
NaveenReddy0427 <naveendeva909@gmail.com>
2024-08-18 21:46:33 +0530

    integrate register component with backend api to register

--- src/vendorDashboard/components/forms/Register.jsx
+++ src/vendorDashboard/components/forms/Register.jsx
@@ -1,17 +1,56 @@
+import { useState } from "react"
+import { API_URL } from "../../data/apiPath.js"
 
 const Register = () => {
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [showPassword, setShowPassword] = useState("")
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        try {
+            const response = await fetch(`${API_URL}/vendor/register`, {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({username, email, password})
+            })
+    
+            const data = await response.json()
+            if(response.ok){
+                console.log(data)
+                setUsername("")
+                setEmail("")
+                setPassword("")
+                alert("Vendor registered successfully");
+            }else{
+                alert("Registration Failed, Contact Admin")
+            }
+        } catch (error) {
+            console.error("Registration failed", error);
+            alert("Registration failed");
+        }
+        
+
+    }
+
   return (
     <div className="registerSection">
-        <form className="authForm">
+        <form className="authForm" onSubmit={handleSubmit}>
             <h3>Vendor Regiter</h3>
             <label>Username</label>
-            <input type="text" placeholder="Enter your username"/><br/>
+            <input type="text" name="username" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your username"/><br/>
             <label>Email</label>
-            <input type="email" placeholder="Enter your email"/><br/>
+            <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter your email"/><br/>
             <label>Password</label>
-            <input type="password" placeholder="Enter your password"/><br/>
+            <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter your password"/><br/>
             <div className="btnSubmit">
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </div>
         </form>
     </div>
--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,1 +1,3 @@
 export const API_URL = "http://localhost:4001"
+
+// export const API_URL = "https://swiggy-app-r6f8.onrender.com"
